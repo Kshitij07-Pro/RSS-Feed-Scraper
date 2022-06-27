eeb3e2268675b52a3406ca7f5b17f937ecb9c885
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-28 00:35:43 +0530

    Update scraping.py

--- web_scraping_example/scraping.py
+++ web_scraping_example/scraping.py
@@ -4,13 +4,15 @@ from bs4 import BeautifulSoup # xml parsing
 import json # exporting to files
 
 # scraping function
-def any_rss():
+def any_rss(URL):
 
     try:
-        feed_url = ''
+        feed_url = 'URL'
         r = requests.get(feed_url)
         soup = BeautifulSoup(r.content, 'html')
-
+        print(soup.prettify())
     except Exception as e:
         print('The scraping job failed. See exception:')
         print(e)
+
+any_rss(<URL_HERE>)
