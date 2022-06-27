ba2f8374f96d4db91f1fac8d7c80ff66cf5bc021
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-27 23:42:15 +0530

    Update scraping.py

--- web_scraping_example/scraping.py
+++ web_scraping_example/scraping.py
@@ -1,1 +1,16 @@
 
+import requests # pulling data
+from bs4 import BeautifulSoup # xml parsing
+import json # exporting to files
+
+# scraping function
+def any_rss():
+
+    try:
+        URL = ''
+        r = requests.get(URL)
+        soup = BeautifulSoup(r.content, 'html')
+        print(soup.prettify())
+    except Exception as e:
+        print('The scraping job failed. See exception:')
+        print(e)
