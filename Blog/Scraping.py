a43174b0f1447460e74caba515f7b52349e87259
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-28 17:10:01 +0530

    Create Scraping.py

---
+++ Blog/Scraping.py
@@ -0,0 +1,45 @@
+# Scraping https://vaibhavkumar.hashnode.dev/rss.xml
+
+def get_posts_details(rss=None):
+    if rss is not None:
+        import feedparser
+        # parsing blog feed
+        blog_feed = feedparser.parse(rss)
+        # getting lists of blog entries
+        posts = blog_feed.entries
+        # dictionary for holding posts details
+        posts_details = {"Blog title" : blog_feed.feed.title,
+                         "Blog link" : blog_feed.feed.link}
+        post_list = []
+
+        for post in posts:
+            temp = dict()
+            # if any post doesn't have information then throw error.
+            try:
+                temp["title"] = post.title
+                temp["link"] = post.link
+                temp["author"] = post.author
+                temp["time_published"] = post.published
+                temp["tags"] = [tag.term for tag in post.tags]
+                temp["authors"] = [author.name for author in post.authors]
+                temp["summary"] = post.summary
+            except:
+                pass
+            post_list.append(temp)
+        # storing lists of posts in the dictionary
+        posts_details["posts"] = post_list
+
+        return posts_details # returning the details which is dictionary
+    else:
+        return None
+
+import json
+
+feed_url = "https://vaibhavkumar.hashnode.dev/rss.xml"
+data = get_posts_details(rss = feed_url) # return blogs data as a dictionary
+
+if data:
+# printing as a json string with indentation level = 2
+    print(json.dumps(data, indent=2))
+else:
+    print("None")
