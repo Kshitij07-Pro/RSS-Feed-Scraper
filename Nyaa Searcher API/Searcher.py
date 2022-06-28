eb62f9f443e78df16dba29dbed8feddb7cac014d
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-29 01:08:06 +0530

    Update Searcher.py

--- Nyaa Searcher API/Searcher.py
+++ Nyaa Searcher API/Searcher.py
@@ -1,1 +1,25 @@
+from autoscraper import AutoScraper
+from flask import Flask, request
 
+scraper = AutoScraper()
+scraper.load('path')
+app = Flask(__name__)
+
+def get_result(search_query):
+    url = 'https://www.nyaa.si/?q=%s' % search_query
+    result = scraper.get_result_similar(url, group_by_alias=True)
+    return _aggregate_result(result)
+
+def _aggregate_result(result):
+    final_result = []
+    for i in range(len(list(result.values())[0])):
+        final_result.append({alias: result[alias][i] for alias in result})
+    return final_result
+
+@app.route('/', methods=['GET'])
+def search_api():
+    query = request.args.get('q')
+    return dict(result=get_result(query))
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
