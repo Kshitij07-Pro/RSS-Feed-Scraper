60bc2c134e95be17c59f9c8f7ebadbe2fc6a689f
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-28 08:52:45 +0530

    Update Scraping.py

--- HackerNews/Scraping.py
+++ HackerNews/Scraping.py
@@ -12,7 +12,7 @@ def hackernews_rss():
         # execute my request, parse the data using XML
         # parser in BS4
         r = requests.get('https://news.ycombinator.com/rss')
-        return print('The scraping job succeeded: ', r.status_code)
+        return print('The scraping job succeeded: ', r.status_code) # 200 is OK, 404 is Not Found
     except Exception as e:
             print('The scraping job failed. See exception:')
             print(e)
