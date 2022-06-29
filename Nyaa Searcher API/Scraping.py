06fb01b0098d7bbc4a1f9072bc01b8c7a05b6048
Kshitij07-Pro <85276195+Kshitij07-Pro@users.noreply.github.com>
2022-06-29 10:27:54 +0530

    Update Scraping.py

--- Nyaa Searcher API/Scraping.py
+++ Nyaa Searcher API/Scraping.py
@@ -3,8 +3,8 @@
 from autoscraper import AutoScraper
 
 # if you want to have same rule ids for each run
-# import random
-# random.seed(0)
+import random
+random.seed(0)
 
 url = 'https://nyaa.si/?q=Naruto'
 wanted_list = ["[YakuboEncodes] Boruto - Naruto Next Generations - 255 [1080p 10bit][x265 HEVC][Multi-Subs]", "https://nyaa.si/download/1547052.torrent", "22", "0", "196.2 MiB"]
@@ -14,8 +14,8 @@ scraper.build(url, wanted_list)
 
 scraper.get_result_similar(url, grouped=True)
 
-scraper.set_rule_aliases = ({"rule_c9xn": "Title", "rule_v6ms": "Torrent Link", "rule_gyyj": "Seeders", "rule_v6gu": "Leechers", "rule_n0ey": "Size"})
-scraper.keep_rules(['rule_c9xn', 'rule_v6ms', 'rule_gyyj', 'rule_v6gu', 'rule_n0ey'])
+scraper.set_rule_aliases = ({"rule_y0cq": "Title", "rule_65zt": "Torrent Link", "rule_4wn6": "Seeders", "rule_isig": "Leechers", "rule_3d0m": "Size"})
+scraper.keep_rules(['rule_y0cq', 'rule_65zt', 'rule_4wn6', 'rule_isig', 'rule_3d0m'])
 
 # to save data
 scraper.save('./Nyaa-search-result.txt')
